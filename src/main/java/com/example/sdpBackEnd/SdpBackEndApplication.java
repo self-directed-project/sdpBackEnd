1db194344285c072b6c7839af63249a8569d04d4
HMWG <nurung22222@gmail.com>
2022-09-09 14:26:46 +0900

    ✨ feat : First Commit

---
+++ src/main/java/com/example/sdpBackEnd/SdpBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.sdpBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdpBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdpBackEndApplication.class, args);
+	}
+
+}
