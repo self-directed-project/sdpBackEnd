9e653ec5b69a28becca96dcb21ef8c7253bc0953
SunMi <wywudi@naver.com>
2022-09-19 02:41:38 +0900

    repository 구현

--- src/main/java/com/example/sdpBackEnd/repository/MemberRepository.java
+++ src/main/java/com/example/sdpBackEnd/repository/MemberRepository.java
@@ -2,8 +2,12 @@ package com.example.sdpBackEnd.repository;
 
 import com.example.sdpBackEnd.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    @Query("select m from Member m where username = :username and password = :password")
+    Member findMember(@Param("username") String username, @Param("password") String password);
 }
