7ba46990526ca2044fa969cee70b680d52f961d5
SunMi <wywudi@naver.com>
2022-09-19 21:21:05 +0900

    service기본생성

--- src/main/java/com/example/sdpBackEnd/service/MemberService.java
+++ src/main/java/com/example/sdpBackEnd/service/MemberService.java
@@ -15,13 +15,14 @@ public class MemberService {
 
     //추후에 spring security 적용
     @Transactional
-    public Long save(final MemberRequestDto param) {
+    public Long login(final MemberRequestDto param) {
 
         try {
             return memberRepository.findMember(param.getUsername(), param.getPassword())
                     .getId();
         } catch (Exception e) {
-            System.out.println("id와 password를 확인해주세요");
+            //에러-customException처리,errorcode생성,errorResponse 추가
+//            System.out.println("id와 password를 확인해주세요"); -테스트 진행시
             return null;
         }
 
