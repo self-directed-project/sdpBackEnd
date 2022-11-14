1dc312834b06e2ca23afb8ca451f58da69e24a3e
HMWG <nurung22222@gmail.com>
2022-11-14 23:12:03 +0900

    :bug: fix : 공용디비 관련 수정 및 BaseTimeEntity 수정

--- src/main/java/com/example/sdpBackEnd/entity/BaseTimeEntity.java
+++ src/main/java/com/example/sdpBackEnd/entity/BaseTimeEntity.java
@@ -19,21 +19,10 @@ public class BaseTimeEntity {
 
     @CreatedDate
     @Column(updatable = false, nullable = false)
-    private String createdDate;
+    private LocalDateTime createdDate;
 
     @LastModifiedDate
     @Column(nullable = true)
-    private String lastModifiedDate;
-
-    @PrePersist
-    public void onPrePersist(){
-        this.createdDate = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss"));
-        this.lastModifiedDate = this.createdDate;
-    }
-
-    @PreUpdate
-    public void onPreUpdate(){
-        this.lastModifiedDate = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss"));
-    }
+    private LocalDateTime lastModifiedDate;
 
 }
