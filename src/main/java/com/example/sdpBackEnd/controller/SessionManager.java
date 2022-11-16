ffc0aa91a7cc44d218d33fe8e2ab706791e0bbc9
SunMi <wywudi@naver.com>
2022-11-17 01:53:22 +0900

    style:세션반환값 변경

--- src/main/java/com/example/sdpBackEnd/controller/SessionManager.java
+++ src/main/java/com/example/sdpBackEnd/controller/SessionManager.java
@@ -15,15 +15,13 @@ import javax.servlet.http.HttpSession;
 public class SessionManager {
 
 
-  public static final String SESSION_COOKIE_NAME = "mySessionId";
+    public static final String SESSION_COOKIE_NAME = "mySessionId";
 
     //session 생성,보관
-    public void createSession(HttpServletRequest request,Member member) {
+    public void createSession(HttpServletRequest request, Member member) {
 
-        HttpSession session=request.getSession();
+        HttpSession session = request.getSession();
         session.setAttribute(SESSION_COOKIE_NAME, member);
-        System.out.println(member.getName()+"의 세션 id :"+session.getId());
-
     }
 
     //유저의 session 조회 - 클라이언트 재접속 시
@@ -33,32 +31,28 @@ public class SessionManager {
         Member loginMember;
 
         //받아온 세션이 없거나 등록된 세션이 아닌 경우
-        try{
+        try {
             session = request.getSession(false);
-            System.out.println(session.getId());
+            System.out.println(session);
             loginMember = (Member) session.getAttribute(SESSION_COOKIE_NAME);
-        }catch (Exception e){
-            System.out.println("/등록된 세션이 없습니다. 로그인페이지로 이동");
-                throw new CustomException(StatusEnum.GET_NOT_FOUND_SID);
+        } catch (Exception e) {
+            throw new CustomException(StatusEnum.GET_NOT_FOUND_SID);
         }
 
-            System.out.println("/요청페이지로 이동\t" + loginMember.getId() + ":" + loginMember.getName() + "\tloginCheck");
 
-            return loginMember.getId();
+        return loginMember.getId();
 
     }
 
-    public void Logout(HttpServletRequest request){
+    //로그아웃 - session기한 만료
+    public void Logout(HttpServletRequest request) {
 
         HttpSession session;
 
-
         try {
             session = request.getSession(false);
-            System.out.println(session.getId());
 
-        }catch (Exception e){
-            System.out.println("/등록된 세션이 없습니다. 로그인페이지로 이동");
+        } catch (Exception e) {
             throw new CustomException(StatusEnum.GET_NOT_FOUND_SID);
         }
 
